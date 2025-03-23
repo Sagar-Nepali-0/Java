792e6d26e2df96532e4c837d94e9e8b03482f28d
Sagar Nepali <sagarnepali1100@gmail.com>
2025-03-23 14:40:34 +0545

    Java File upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+class HelloWorld{
+    public static void main(String [] args){
+        String name = "Sagar Nepali";
+
+        System.out.println("Hello, World!");
+        System.out.println("My name is " + name+".");
+    }
+}
