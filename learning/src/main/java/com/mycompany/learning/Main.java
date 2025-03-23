cb1ece380bb7b74de739a48a6b9d66e0b388b3f4
Sagar Nepali <sagarnepali1100@gmail.com>
2025-03-23 18:47:05 +0545

    car3, variable year quotation remove

--- learning/src/main/java/com/mycompany/learning/Main.java
+++ learning/src/main/java/com/mycompany/learning/Main.java
@@ -12,7 +12,7 @@ public class Main {
     static void carFunction(){
         Car car1 = new Car("Rolls Royce", "Rolls-Royce Boat Tail", 2022, "Blue", 28000000, false);
         Car car2 = new Car("Toyota", "Camry", 2022, "Black", 30000, true);
-        Car car3 = new Car("Honda", "Civic", "2023", "Red", 28000, false);
+        Car car3 = new Car("Honda", "Civic", 2023, "Red", 28000, false);
         car1.carDetails();
         car2.carDetails();
         car3.carDetails();
