7173197d4ba402f3efe05ea13cc1bef8b6c219d6
Sagar Nepali <sagarnepali1100@gmail.com>
2025-03-24 14:05:11 +0545

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,8 +0,0 @@
-class HelloWorld{
-    public static void main(String [] args){
-        String name = "Sagar Nepali";
-
-        System.out.println("Hello, World!");
-        System.out.println("My name is " + name+".");
-    }
-}
